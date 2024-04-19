e3552357dafacec2cb142d12a2fa6d77a635d97e
Shubh-Bobade <shubhambobade301@gmail.com>
2024-04-19 14:25:58 +0530

    write a new method, start new project

--- GitDemoProject/src/com/test/TestClass.java
+++ GitDemoProject/src/com/test/TestClass.java
@@ -2,9 +2,12 @@ package com.test;
 
 public class TestClass {
 
+	public void update() {
+		System.out.println("start new project ");
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to git hub projects ");
-
+		
 	}
 
 }
