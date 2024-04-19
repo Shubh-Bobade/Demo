e98f636637223ccf8e2d279555159ea048c22f16
Shubh-Bobade <shubhambobade301@gmail.com>
2024-04-19 14:55:44 +0530

    add new classes cart and payment

---
+++ GitDemoProject/src/com/product/Cart.java
@@ -0,0 +1,11 @@
+package com.product;
+
+public class Cart {
+
+	public static void main(String[] args) {
+
+		System.out.println("inside cart class");
+		
+	}
+
+}
---
+++ GitDemoProject/src/com/product/Payment.java
@@ -0,0 +1,10 @@
+package com.product;
+
+public class Payment {
+
+	public static void main(String[] args) {
+
+		System.out.println("Inside payment class");
+	}
+
+}
