3fdce20984818dfb530b330137cf99e7935af0f3
Shubh-Bobade <shubhambobade301@gmail.com>
2024-04-19 15:52:37 +0530

    complete the cart process

--- GitDemoProject/src/com/product/Cart.java
+++ GitDemoProject/src/com/product/Cart.java
@@ -1,11 +1,27 @@
 package com.product;
 
+import java.util.Scanner;
+
 public class Cart {
+	public void cart(int input) {
+		
+		if(input > 0) {
+			System.out.println("Select Payment method");			
+		} else {
+			System.out.println("Chooese atleast one pic");
+		}
+		
+	}
 
 	public static void main(String[] args) {
 
 		System.out.println("inside cart class");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Quantity ");
+		int input = sc.nextInt();
 		
+		Cart obj = new Cart();
+		obj.cart(input);
 	}
 
 }
